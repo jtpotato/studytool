9760c80daea65be4deca9532e5255792b6259ffb
Joel <58995538+jtpotato@users.noreply.github.com>
2023-12-13 13:41:04 +1100

    Initial Commit

---
+++ studytool/studytoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  studytoolApp.swift
+//  studytool
+//
+//  Created by Joel Tan on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct studytoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
